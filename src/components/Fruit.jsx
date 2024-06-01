8224505533309c63a92f289beba3e79f261fbeb6
haniffauzann55@gmail.com <haniffauzann55@gmail.com>
2024-06-01 22:09:25 +0700

    Create Fruit Card

--- src/components/Fruit.jsx
+++ src/components/Fruit.jsx
@@ -1,3 +1,24 @@
+import { MessageCircle } from "lucide-react";
+import { Heart } from "lucide-react";
+
 export default function Fruit(){
-    
+    return(
+        <>
+        <div className="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+        <img className="w-full h-48 object-cover" src="https://via.placeholder.com/300" alt="Buah" />
+        <div className="p-6">
+            <h1 className="text-2xl font-bold text-gray-800 mb-2">Nama Buah</h1>
+            <p className="text-gray-600 mb-4">Tiga Kata Deskripsi</p>
+            <div className="flex items-center justify-between">
+                <button className="text-blue-500 hover:text-blue-700">
+                <Heart />
+                </button>
+                <button className="text-green-500 hover:text-green-700">
+                <MessageCircle />
+                </button>
+            </div>
+        </div>
+    </div>
+        </>
+    )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,30 @@
-export default function Header(){
-    return(
-        <>
-        <header className="bg-blue-600 text-white p-6 shadow-md">
+import { Grape } from "lucide-react";
+
+export default function Header() {
+  return (
+    <>
+      <header className="bg-blue-600 text-white p-6 shadow-md">
         <div className="container mx-auto flex justify-between items-center">
-            <h1 className="text-3xl font-bold">My Awesome Fruits</h1>
-            <nav className="flex space-x-4">
-                <a href="#" className="text-white hover:text-gray-200">Home</a>
-                <a href="#" className="text-white hover:text-gray-200">About</a>
-                <a href="#" className="text-white hover:text-gray-200">Services</a>
-                <a href="#" className="text-white hover:text-gray-200">Contact</a>
-            </nav>
+          <div className="flex items-center gap-5">
+            <Grape />
+            <h1 className="text-3xl font-bold ">My Awesome Fruits</h1>
+          </div>
+          <nav className="flex space-x-4">
+            <a href="#" className="text-white hover:text-gray-200">
+              Home
+            </a>
+            <a href="#" className="text-white hover:text-gray-200">
+              About
+            </a>
+            <a href="#" className="text-white hover:text-gray-200">
+              Services
+            </a>
+            <a href="#" className="text-white hover:text-gray-200">
+              Contact
+            </a>
+          </nav>
         </div>
-    </header>
-        </>
-    )
+      </header>
+    </>
+  );
 }
