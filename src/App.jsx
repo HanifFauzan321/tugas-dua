f6c07cc844433592e8e27e9a2cf04edabde58db1
haniffauzann55@gmail.com <haniffauzann55@gmail.com>
2024-06-03 20:29:35 +0700

    Ubah info

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,68 @@
-import Fruit from "./components/Fruit"
-import Header from "./components/Header"
+import Fruit from "./components/Fruit";
+import Header from "./components/Header";
 
 function App() {
-
   return (
     <>
-    <Header/>
-    <Fruit/>
+      <Header />
+      <div className="flex gap-5 flex-wrap bg-gradient-to-r from-blue-100 to-blue-500 p-6">
+        {fruits.map((data) => (
+          <Fruit key={data} fruitsProps={data} />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+const fruits = [
+  {
+    id: 1,
+    name: "Avocado",
+    price: 15000,
+    category: "Tropical",
+    description: "This fruit is green and tasty",
+    photo: "src/image/Alpukat.jpg",
+  },
+  {
+    id: 2,
+    name: "Apple",
+    price: 17000,
+    category: "Tropical",
+    description: "The fruit is red and fresh",
+    photo: "src/image/Apel.jpg",
+  },
+  {
+    id: 3,
+    name: "Orange fruit",
+    price: 10000,
+    category: "Tropical",
+    description: "the fruit is orange and sweet",
+    photo: "src/image/Jeruk.jpg",
+  },
+  {
+    id: 4,
+    name: "Watermelon",
+    price: 16000,
+    category: "Tropical",
+    description: "This fruit has green skin and sweet red contents",
+    photo: "src/image/Semangka.jpg",
+  },
+  {
+    id: 5,
+    name: "Banana",
+    price: 20000,
+    category: "Tropical",
+    description: "Kuning dan Manis",
+    photo: "src/image/Pisang.jpg",
+  },
+  {
+    id: 6,
+    name: "Buah Kelengkeng",
+    price: 22000,
+    category: "Tropical",
+    description: "Coklat Manis",
+    photo: "src/image/Kelengkeng.jpg",
+  },
+];
+
+export default App;
--- src/components/Fruit.jsx
+++ src/components/Fruit.jsx
@@ -1,24 +1,48 @@
+/* eslint-disable react/prop-types */
+import { Info } from "lucide-react";
+import { Send } from "lucide-react";
 import { MessageCircle } from "lucide-react";
 import { Heart } from "lucide-react";
 
-export default function Fruit(){
-    return(
-        <>
-        <div className="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-hidden">
-        <img className="w-full h-48 object-cover" src="https://via.placeholder.com/300" alt="Buah" />
+export default function Fruit({fruitsProps}) {
+
+  const showInfo = (fruitsProps) => {
+    alert(` Name          : ${fruitsProps.name} \n Price            : IDR.${fruitsProps.price}, \n Category     : ${fruitsProps.category}\n Description : ${fruitsProps.description}`);
+  }
+
+  const showLike = (fruitsProps) => {
+    alert(`I Like ${fruitsProps.name}`);
+  }
+
+  return (
+    <>
+      <div className="min-w-80 mx-auto my-10 bg-blue-200 rounded-lg shadow-md overflow-hidden">
+        <img
+          className="w-full h-48 object-cover"
+          src={fruitsProps.photo}
+          alt="Buah"
+        />
         <div className="p-6">
-            <h1 className="text-2xl font-bold text-gray-800 mb-2">Nama Buah</h1>
-            <p className="text-gray-600 mb-4">Tiga Kata Deskripsi</p>
-            <div className="flex items-center justify-between">
-                <button className="text-blue-500 hover:text-blue-700">
-                <Heart />
-                </button>
-                <button className="text-green-500 hover:text-green-700">
-                <MessageCircle />
-                </button>
-            </div>
+          <h1 className="text-2xl font-bold text-gray-950 mb-2">{fruitsProps.name}</h1>
+          <p className="text-gray-900 mb-4">{fruitsProps.description}</p>
+          <div className="flex items-center justify-between">
+            <button className="text-blue-400 hover:text-blue-700" onClick={() => showInfo(fruitsProps)}>
+              <Info />
+            </button>
+            <button className="text-pink-400 hover:text-pink-700 " onClick={() => showLike(fruitsProps)}>
+              <Heart />
+            </button>
+            <button className="text-green-400 hover:text-green-700">
+              <MessageCircle />
+            </button>
+            <button className="text-slate-400 hover:text-slate-800">
+              <Send />
+            </button>
+          </div>
         </div>
-    </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
+
+  
